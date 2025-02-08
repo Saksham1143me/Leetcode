4f9be3ac203ec0e79ba7a91a01ec176f7fb66cd4
Saksham1143me <150987031+Saksham1143me@users.noreply.github.com>
2025-02-08 10:56:55 +0530

    Time: 177 ms (5.63%) | Memory: 196.9 MB (20.00%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <set>
+
+class NumberContainers {
+public:
+    unordered_map<int, int> indexToNumber;  
+    map<int, set<int>> numberToIndices; 
+    NumberContainers() {}
+
+    void change(int index, int number) {
+        if (indexToNumber.count(index)) {
+            int oldNumber = indexToNumber[index];
+            numberToIndices[oldNumber].erase(index);
+            if (numberToIndices[oldNumber].empty()) {
+                numberToIndices.erase(oldNumber);
+            }
+        }
+
+        indexToNumber[index] = number;
+        numberToIndices[number].insert(index);
+    }
+
+    int find(int number) {
+        if (numberToIndices.count(number) && !numberToIndices[number].empty()) {
+            return *numberToIndices[number].begin();
+        }
+        return -1;
+    }
+};

afb74631cd21941ccef4379cdba958b7114ab772
Saksham1143me <150987031+Saksham1143me@users.noreply.github.com>
2025-02-14 15:57:40 +0530

    Time: 16 ms (82.06%) | Memory: 77.9 MB (77.84%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,18 +1,15 @@
 class ProductOfNumbers {
 public:
     vector<int> prefixProduct;
-    int lastZeroIndex;
 
     ProductOfNumbers() {
         prefixProduct.push_back(1);
-        lastZeroIndex = -1;
     }
     
     void add(int num) {
         if (num == 0) {
             prefixProduct.clear();
             prefixProduct.push_back(1);
-            lastZeroIndex = prefixProduct.size();  
         } else {
             prefixProduct.push_back(prefixProduct.back() * num);
         }
